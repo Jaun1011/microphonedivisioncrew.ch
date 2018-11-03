948ac2edeafe9b02ce151fd4d6cb0d05ec6d87d6
Jan(U117292) <jan.kuonen.93@gmail.com>
2018-11-03 06:09:42 +0100

    github buiild

--- config/index.js
+++ config/index.js
@@ -43,7 +43,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/microphonedivisioncrew.ch',
+    assetsPublicPath: '/',
 
     /**
      * Source Maps
