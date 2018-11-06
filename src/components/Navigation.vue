dbde0c67e3ebadfffb30e5265b508f2d0984d456
Jaun1011 <jan.kuonen.93@gmail.com>
2018-11-06 20:47:27 +0100

    build II

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,80 +1,81 @@
 <template>
-  <div class="navigation">
-    <div class="container">
-      <img src="static/icons/navigation.svg">
-      <ul>
-        <li v-for="i in nav"
-            v-on:click="route(i.route)"
-            v-bind:class="isActive(i.route)" >
-          {{i.label}}
-        </li>
-      </ul>
+    <div class="navigation">
+        <div class="container">
+            <img src="static/icons/navigation.svg">
+            <ul>
+                <li v-for="i in nav"
+                    v-on:click="route(i.route)"
+                    v-bind:class="isActive(i.route)">
+                    {{i.label}}
+                </li>
+            </ul>
+        </div>
     </div>
-  </div>
 </template>
 
 <style scoped>
-  .container {
-    position: relative;
-  }
-
-  .active, li:hover {
-    color: red;
-  }
-  img {
-    width: 180%;
-    margin-left: -80%;
-    animation: spin 40s linear infinite;
-  }
-
-     @keyframes spin {
-    100% {
-      -webkit-transform: rotate(360deg);
-      transform: rotate(360deg);
+    .container {
+        position: relative;
     }
-  }
 
-  ul {
-    position: absolute;
-    top: 40%;
-    left: 50%;
-    list-style-type: none;
-  }
+    .active, li:hover {
+        color: red;
+    }
 
-  li {
-    color: white;
-    font-size: 2em;
-    padding-bottom: 10px;
-  }
+    img {
+        width: 180%;
+        margin-left: -80%;
+        animation: spin 40s linear infinite;
+    }
+
+    @keyframes spin {
+        100% {
+            -webkit-transform: rotate(360deg);
+            transform: rotate(360deg);
+        }
+    }
+
+    ul {
+        position: absolute;
+        top: 40%;
+        left: 50%;
+        list-style-type: none;
+    }
+
+    li {
+        color: white;
+        font-size: 2em;
+        padding-bottom: 10px;
+    }
 
 
 </style>
 
 <script>
-  export default {
-    name: 'Navigation',
-    data() {
-      return {
-        activeRoute: '/',
-        nav: [
-          {label: "Songs", route: "/songs"},
-          {label: "Crew", route: "/crew"},
-          {label: "Pics", route: "/pictures"},
-        ]
-      }
-    },
-    methods: {
-      route: function (route) {
-        this.activeRoute = route;
+    export default {
+        name: 'Navigation',
+        data() {
+            return {
+                activeRoute: '/',
+                nav: [
+                    {label: "Songs", route: "/songs"},
+                    {label: "Crew", route: "/crew"},
+                    {label: "Pics", route: "/pictures"},
+                ]
+            }
+        },
+        methods: {
+            route: function (route) {
+                this.activeRoute = route;
 
-        this.$router.push(route)
-      },
-      isActive(route) {
-        return this.activeRoute === route ?
-          "active" : "";
-      }
+                this.$router.push(route)
+            },
+            isActive(route) {
+                return this.activeRoute === route ?
+                    "active" : "";
+            }
+        }
     }
-  }
 </script>
 
 
